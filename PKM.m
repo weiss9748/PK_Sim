9f07d7d9733223d827eca8296ea9af6229576d81
Abdullah Weiss <abdullah.weiss@students.tamuk.edu>
2018-02-05 22:28:13 -0600

    Add files via upload

--- PKM.m
+++ PKM.m
@@ -1,4 +1,4 @@
-function PKM
+%function PKM
 clear; clf;close all; clc
 
 disp('--------------------------------------------------------------------------------------------------------------')
@@ -47,16 +47,44 @@ Step_Size=cell2mat(co2(18)); %auto step size response (Y or N)
 NIntr=str2double(cell2mat(co2(20))); %number of interest points
 IPts=str2double(co(21,2:NIntr+1)); %interest pts
 
-ti=(0:max(IPts));
+increment=0.01;
+ti=(0:increment:max(IPts)); %time vector for the simulation
 C0=((B./(A.*La)).*n0).'; %initial delayed neutron precursor densities for all the groups
 y0=[n0;C0]; %initial y column vector
-dfdy=[((double(subs(rho,ti(1)))-Beta)/A) La; (B(1)/A) -La(1) 0 0 0 0 0;
+tip=zeros(1,length(ti)*max(rxnum));
+rhot=zeros(1,length(ti)*max(rxnum));
+g=0;
+i=1;
+
+
+if rxnum>1 % the if rxnum part is under development
+    while max(tip)<max(IPts)
+        g=g+1
+        ix = find(IPts>=tip(i),1);
+        while tip(i)<IPts(ix);
+            rhot(i)=double(subs(rho(g),tip(i)));
+            tip(i)=tip(i)+increment;
+            i=i+1;
+        end
+    end
+    [t,y]=ode23s(@(t,y) [((rhot(tip==t)-Beta)/A) La;
+        (B(1)/A) -La(1) 0 0 0 0 0;
         (B(2)/A) 0 -La(2) 0 0 0 0;
         (B(3)/A) 0 0 -La(3) 0 0 0;
         (B(4)/A) 0 0 0 -La(4) 0 0;
         (B(5)/A) 0 0 0 0 -La(5) 0;
-        (B(6)/A) 0 0 0 0 0 -La(6)];
-[t,y]=ode23s(@(t,y) dfdy*y,ti,y0);
+        (B(6)/A) 0 0 0 0 0 -La(6)]*y,ti,y0); %solving the stiff ODE
+elseif rxnum<1
+    disp('Number of reactivity steps must be greater than 1')
+else
+    [t,y]=ode23s(@(t,y) [((double(subs(rho,t))-Beta)/A) La;
+        (B(1)/A) -La(1) 0 0 0 0 0;
+        (B(2)/A) 0 -La(2) 0 0 0 0;
+        (B(3)/A) 0 0 -La(3) 0 0 0;
+        (B(4)/A) 0 0 0 -La(4) 0 0;
+        (B(5)/A) 0 0 0 0 -La(5) 0;
+        (B(6)/A) 0 0 0 0 0 -La(6)]*y,ti,y0); %solving the stiff ODE
+end
 
 %figure for the neutron density (n) graph
 FIG1=figure('Name','Neutron Density','NumberTitle','off');
@@ -75,7 +103,17 @@ title('Neutron Precursor Density')
 plot(t,y(:,2),t,y(:,3),t,y(:,4),t,y(:,5),t,y(:,6),t,y(:,7))
 xlabel('t/s')
 ylabel('C_i(t)')
+legend('C_1(t)','C_2(t)','C_3(t)','C_4(t)','C_5(t)','C_6(t)')
 movegui(FIG2,'east');
 hold off
-toc
+
+dor=zeros(length(IPts),NGr+2);
+for i=1:length(IPts)
+    dire=find(t==IPts(i));
+    dor(i,2:end)=y(dire,:);
 end
+dor(:,1)=IPts';
+disp(dor)
+
+toc %ends the timer for CPU time
+%end
